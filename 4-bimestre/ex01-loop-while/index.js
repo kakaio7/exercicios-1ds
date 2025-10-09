085a5e0094c1055cd6a442fe22371db45841a062
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-10-09 17:48:58 +0000

    terminado!

--- 4-bimestre/ex01-loop-while/index.js
+++ 4-bimestre/ex01-loop-while/index.js
@@ -1,4 +1,5 @@
 // CRIE UMA LÓGICA AQUI
+
 let resultado = 1
 while(resultado <= 10){
     console.log(resultado)
@@ -6,5 +7,6 @@ while(resultado <= 10){
 }
 resultado -= 1
 console.log(`Seu resultado é ${resultado}`)
+
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
