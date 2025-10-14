c35ce6303dc5004807bb90c3d7db5fa4aa64d6ff
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-10-14 14:46:44 +0000

    resolvido

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,8 @@
 // CRIE UMA LÓGICA AQUI
-
+let resultado = 0
+for(let i = 0; i <= 10; i++){
+    resultado = resultado + i 
+    console.log(resultado)
+}
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
