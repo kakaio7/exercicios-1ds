b0d49bc2c9054bc72a5692a70c56fbe0a6a78c5f
Kaua Gomes <kaua.bezerra7@aluno.ce.gov.br>
2025-11-13 20:55:11 +0000

    corrigido!

--- 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
+++ 4-bimestre/projetos/projeto7-catalogo-de-filmes/index.js
@@ -38,7 +38,7 @@ console.log(`
     Filme mais bem avaliado:
     ${filme[`filme${filmemaior}`].titulo} (${maior})
 
-    media geral das avaliaçoes:${media}
+    media geral das avaliaçoes:${media.toFixed(2)}
     `)
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = { filme1, filme2, filme3, filme4, filme5, filme6, filme7, filme8, filme9, filme10 }
